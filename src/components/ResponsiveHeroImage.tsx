52fc709c6a0cf6991621d7f9b627b43b06ebf431
practicalmess <hacksid.official@gmail.com>
2025-04-25 19:40:07 +0000

    Deploying to main from @ pmrp-boston/pmrp-boston.github.io@38e3979c1f3e6c01b03cefb80de98cb6e4bf2b83 🚀

--- src/components/ResponsiveHeroImage.tsx
+++
@@ -1,9 +0,0 @@
-const ResponsiveHeroImage = () => {
-  return (
-    <div className='heroBannerImg'>
-      <img src="./DangerousBannerSimple.png" alt="The Post Meridian Radio Players Presents: Dangerous Adventures!" className="heroBannerImage" />
-    </div>
-  );
-};
-
-export default ResponsiveHeroImage;
